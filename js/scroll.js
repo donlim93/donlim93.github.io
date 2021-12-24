ed9ce8268bf08898edb0ca4b5b7c25ea79549eb3
donlim93 <89227601+donlim93@users.noreply.github.com>
2021-12-24 08:09:15 -0600

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -9,9 +9,7 @@ window.onscroll = function () {
 function scrollFunction() {
   if (
     document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20 ||
-    document.body.scrollTop == 0 ||
-    document.documentElement.scrollTop == 0
+    document.documentElement.scrollTop > 20
   ) {
     mybutton.style.display = "block";
   } else {
