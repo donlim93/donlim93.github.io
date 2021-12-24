027336f23edcf414bab0bf79dfc67034d39cdb92
donlim93 <89227601+donlim93@users.noreply.github.com>
2021-12-24 07:51:15 -0600

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -2,17 +2,23 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ||document.body.scrollTop == 0) {
-    mybutton.style.display = "block";
-  } else {
+  if (
+    document.body.scrollTop < 20 ||
+    document.documentElement.scrollTop < 20 ||
+    document.body.scrollTop == 0
+  ) {
     mybutton.style.display = "none";
+  } else {
+    mybutton.style.display = "block";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  window.scrollTo({top:0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
